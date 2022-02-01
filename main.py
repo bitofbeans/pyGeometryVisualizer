8da54d9fc9c0adb736352e15f3e80d39eac908c6
bitofbeans <benas4sun@gmail.com>
2022-01-31 22:00:52 -0600

    Added basic GUI frame and cleaned up code

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-import visvis as vv # visualizing
-import numpy as np # math
+import visvis as vv # Visualizer
+import numpy as np # Math functions
+import PySimpleGUI as sg # For GUI
 
 # --- Start Code  --- #
 
-# List of all objects for color map
-objects = []
+fig = vv.figure() # Create figure
+fig.position.w = 700 # Resize window width
+fig.position.h = 500 # Resize window height
 
-# plane creator
-def createPlane(pos, scale):
-    # Thickness of \/
-    scale = list(scale)
-    scale.append(0.25)
+a = vv.subplot(111) # Create axes
+
+objects = [] # List of all objects for color map
+
+# Plane creator
+def createPlane(pos, scale, rot=0):
+    scale = list(scale) 
+    scale.append(0.1) # Thickness of plane
     # Create object
-    obj = vv.solidBox(pos,scale)
-    # Add to color map list
-    objects.append(obj)
-    # Return box object
-    return obj
-    
+    if rot: # If rotation is customized
+        obj = vv.solidBox(pos,scale,rot)
+    else:
+        obj = vv.solidBox(pos,scale)
+    objects.append(obj) # Add to color map list
+    return obj # Return box object
+     
 # Create plane
+ ## (Position, Scale, Rotation)
 plane = createPlane((0, 0, 0), (6, 6))
-plane2 = createPlane((0, 0, 1), (6, 6))
+plane2 = createPlane((0, 0, 1), (6, 6), (0,1,0))
 
-
-
-# Add color map for all objects
-for obj in objects:
-    # Set Color Map
-    N = obj._vertices.shape[0]
-    obj.SetValues( np.linspace(0,1,N) )
-    obj.colormap = vv.CM_JET
-
+for obj in objects: # Add color map for all objects
+    N = obj._vertices.shape[0] # get vertices
+    obj.SetValues( np.linspace(0,1,N) ) # honestly no clue
+    obj.colormap = vv.CM_JET # set colormap to rainbow
 
 # --- Run Program --- #
-# Create axes
-a = vv.gca()
-a.camera = 3 # 1 = Fly, 2 = 2D, 3 = 3D
-
-vv.title('Visualizer') # Set title
 app = vv.use() # Create app instance
-app.Run() # Run main loop
+
+## We don't need an app.Run() 
+## in this because our GUI
+## is acting as the loop
+
+# GUI loop
+run = True
+while run:
+    sg.theme('Dark Blue 3')  # set gui theme
+    # Define layout
+    layout = [
+        [sg.Text('Add Objects                                            ', justification='center')],
+        [sg.Button("Add Plane")],
+        [sg.Button("Add Plane")],
+    ]
+    window = sg.Window('Add Objects', layout) # Open window
+    event, values = window.read() # Get inputs
+    window.close() # Close window
+    
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        run = False # Exit condition
